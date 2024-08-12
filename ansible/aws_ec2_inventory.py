14bf8f9c9a8f4fecbaf72a130a3c6bd49d00ea6b
Eshan-m <thineemadura@gmail.com>
2024-08-12 16:15:53 +0000

    Updated the aws anisiable

--- ansible/aws_ec2_inventory.py
+++ ansible/aws_ec2_inventory.py
@@ -2,45 +2,47 @@
 
 import boto3
 import json
+import sys
 
-def get_instances_by_tag(tag_key, tag_value):
+def get_instances_by_name_patterns(name_patterns):
     ec2 = boto3.client('ec2')
-    response = ec2.describe_instances(
-        Filters=[
-            {
-                'Name': f'tag:{tag_key}',
-                'Values': [tag_value]
-            },
-            {
-                'Name': 'instance-state-name',
-                'Values': ['running']
-            }
-        ]
-    )
+    filters = [
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }
+    ]
     
+    # Build filters for name patterns
+    name_filters = [{'Name': 'tag:Name', 'Values': [pattern]} for pattern in name_patterns]
+    filters.extend(name_filters)
+    
+    try:
+        response = ec2.describe_instances(Filters=filters)
+    except Exception as e:
+        print(f"Error fetching instances: {e}", file=sys.stderr)
+        sys.exit(1)
+        
     instances = []
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
-            instances.append(instance['PublicIpAddress'])
+            public_ip = instance.get('PublicIpAddress')
+            private_ip = instance.get('PrivateIpAddress')
+            if public_ip:
+                instances.append(public_ip)
+            elif private_ip:
+                instances.append(private_ip)
     return instances
 
 def main():
-    web_1_2_ips = get_instances_by_tag('Name', 'web-server-1')
-    web_3_4_ips = get_instances_by_tag('Name', 'web-server-3')
-    web_5_6_ips = get_instances_by_tag('Name', 'web-server-5')
+    # Define all name patterns to match your instances
+    name_patterns = ['*web-server*', '*VM*', '*BASITION*']
+    
+    instances_ips = get_instances_by_name_patterns(name_patterns)
 
     inventory = {
         'all': {
-            'hosts': web_1_2_ips + web_3_4_ips + web_5_6_ips
-        },
-        'web_1_2': {
-            'hosts': web_1_2_ips
-        },
-        'web_3_4': {
-            'hosts': web_3_4_ips
-        },
-        'web_5_6': {
-            'hosts': web_5_6_ips
+            'hosts': instances_ips
         }
     }
 
