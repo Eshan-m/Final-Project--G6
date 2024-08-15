91c7ca6a9db6420b3ecaf680d8b13b9181a4d03c
Akash-Lakshmanan <aku.1d514@gmail.com>
2024-08-15 19:28:02 -0400

    Update output.tf

--- webserver/output.tf
+++ webserver/output.tf
@@ -1,10 +1,5 @@
 # output for webservers
 
-# Output Web Server 1 Details
-output "web_server_1_id" {
-  description = "ID of the first web server instance"
-  value       = aws_instance.web_1.id
-}
 
 output "web_server_1_public_ip" {
   description = "Public IP address of the first web server"
@@ -78,4 +73,9 @@ output "vmprivate_5_id" {
 
 output "lb_dns_name" {
   value = aws_lb.web_lb.dns_name
+}
+# Output Web Server 1 Details
+output "web_server_1_id" {
+  description = "ID of the first web server instance"
+  value       = aws_instance.web_1.id
 }
