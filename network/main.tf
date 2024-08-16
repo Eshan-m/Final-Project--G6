4d178ea1028070d606e5d9cf755540f203758300
Eshan-m <thineemadura@gmail.com>
2024-08-16 03:48:15 +0000

    update in network

--- network/main.tf
+++ network/main.tf
@@ -112,7 +112,7 @@ resource "aws_route_table_association" "public_association_4" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Create a NAT Gateway
+# Create a NAT Gateway in Public Subnet 1
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_subnet_1.id
@@ -131,7 +131,6 @@ resource "aws_eip" "nat" {
   }
 }
 
-
 # Route Table for Private Subnets
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
@@ -147,20 +146,13 @@ resource "aws_route" "private_to_nat" {
   nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
-
 # Associate Route Table with Private Subnets
 resource "aws_route_table_association" "private_association_1" {
   subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_rt.id
 }
 
-
 resource "aws_route_table_association" "private_association_2" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_rt.id
 }
-
-
-
-
-
