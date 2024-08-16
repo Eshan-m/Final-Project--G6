62c48744c6ef1e08bec05e37e124065fbd46f199
Eshan-m <thineemadura@gmail.com>
2024-08-16 02:24:51 +0000

    network main

--- network/main.tf
+++ network/main.tf
@@ -141,6 +141,7 @@ resource "aws_route_table_association" "private_association_1" {
   route_table_id = aws_route_table.private_rt.id
 }
 
+
 resource "aws_route_table_association" "private_association_2" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_rt.id
