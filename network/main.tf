69bb0861fde1a6a8697ed60f27c6f2865aa63826
Eshan-m <thineemadura@gmail.com>
2024-08-16 02:52:05 +0000

    update on the network main

--- network/main.tf
+++ network/main.tf
@@ -131,19 +131,22 @@ resource "aws_eip" "nat" {
   }
 }
 
-/*
+
 # Route Table for Private Subnets
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
   tags = {
     Name = "private-route-table"
   }
 }
-*/
+
+# Route traffic from the private subnets through the NAT Gateway
+resource "aws_route" "private_to_nat" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
 
 # Associate Route Table with Private Subnets
 resource "aws_route_table_association" "private_association_1" {
@@ -157,12 +160,7 @@ resource "aws_route_table_association" "private_association_2" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-# Route traffic from the private subnets through the NAT Gateway
-resource "aws_route" "private_to_nat" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
+
 
 
 
